f296941202f9663084ccab165135f8286eb639f1
KamalDhillon <kkaur7887@gmail.com>
2021-12-18 12:11:07 -0800

    addition

---
+++ src/Addition/GitAddition.java
@@ -0,0 +1,16 @@
+package Addition;
+
+public class GitAddition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Syntax
+		//Datatype variable name
+
+	int num1=5;
+	int num2=10;
+	int addition=num1+num2;
+	System.out.println("addition="+addition);}
+	
+
+}
